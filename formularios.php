9cfdacd5f057044f31198219c8059bfaaefcd804
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2024-10-01 10:10:04 +0200

    añadido para mostrar checkboxes

--- formularios.php
+++ formularios.php
@@ -1,18 +1,13 @@
 <?php
 
 
-  
-  
     $nombre = $_GET["nombre"];
     $apellidos = $_GET["apellidos"];
     
     $sexo = $_GET["sexo"];
     $fecha_nac = $_GET["fecha_nacimiento"];
 
-    if (isset($_GET["notificacion"])){
-      $opcion = $_GET["notificacion"];
-
-    }
+    
 
 
     echo '<h1>Nombre: '. $nombre . '</h1><br><br>';
@@ -20,7 +15,14 @@
     
     echo '<h1>Sexo: '. $sexo . '</h1><br><br>';
     
-    echo '<h1>Fecha nacimineto: '. $fecha_nac . '</h1><br><br>';
-    echo '<h1>Has elegido la opción: '. $opcion .'</h1>';
+    echo '<h1>Fecha de nacimiento: '. $fecha_nac . '</h1><br><br>';
+    
+    if (isset($_GET["organizacion"])){
+        foreach ($_GET["organizacion"] as $opcion) {
+          echo '<h1>Has elegido la opción: '. $opcion .'</h1>';
+
+        }
+    }
+
 
 ?>
